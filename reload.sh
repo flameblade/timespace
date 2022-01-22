e0075cfb1e044a46feca8c3af11ffa895044f2bf
jihoon kim <wlgnsl132@naver.com>
2022-01-22 15:56:33 +0900

    reload.sh

---
+++ reload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "PID Check..."
+CURRENT_PID=$(ps -ef | grep java | grep spacetime* | awk '{print $2}')
+echo "Running PID: {$CURRENT_PID}"
+if $CURRENT_PID [ -z CURRENT_PID ];then
+   echo "Project is not #running"
+else
+   kill -9 $CURRENT_PID;
+   sleep10;
+fi
+echo "Deploy Project...." nohup java -jar /home/serve/timespace/spacetime/spacetime-0.0.1-SNAPSHOT.jar >> /home/serve/timespace/spacetime/spacetime.log & echo "Done"
+
